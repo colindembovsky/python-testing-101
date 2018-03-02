c3f7cf53bd63dfdefd5a8ddeaa883ddd7012b48c
Colin Dembovsky <colindembovsky@gmail.com>
2018-03-01 22:56:38 -0800

    Update test_calc_func.py

--- example-py-pytest/tests/test_calc_func.py
+++ example-py-pytest/tests/test_calc_func.py
@@ -10,10 +10,11 @@ from com.automationpanda.example.calc_func import *
 NUMBER_1 = 3.0
 NUMBER_2 = 2.0
 
-
+"""
 def test_add():
     value = add(NUMBER_1, NUMBER_2)
     assert value == 5.0
+"""
 
 
 def test_subtract():
